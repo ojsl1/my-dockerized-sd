7d2225c086e501624a688c283b4fc17219fe5f94
ojsl1 <ojsl@protonmail.ch>
2025-01-08 17:19:14 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@
 #cp -v ./config.json /A1111/stable-diffusion-webui/config.json
 #cp -v ./ui-config.json /A1111/stable-diffusion-webui/ui-config.json
 
-echo "NOTE: Not directly starting the webui with Dockerfile: RUN python3 launch.py command"
-echo "NOTE: Not directly starting the webui with docker-compose.yaml directive"
-echo "Current pwd is: $(pwd)"
-echo "Succesfully started the copied entrypoint.sh"
+#echo "NOTE: Not directly starting the webui with docker-compose.yaml directive"
+echo "LOG: Current pwd is: $(pwd)"
+echo "LOG: Succesfully started the copied entrypoint.sh"
 exec python3 launch.py --listen --enable-insecure-extension-access --disable-safe-unpickle --opt-sdp-attention
