030858d218689cfa657802359b4da1cb6424e8ce
ojsl1 <ojsl@protonmail.ch>
2025-01-31 11:09:16 +0200

    Update

--- launch-comfy.sh
+++ launch-comfy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # "From ComfyUI README:: Might speed things up at the cost of a very slow initial run"
-#ENV PYTORCH_TUNABLEOP_ENABLED=1
+#export PYTORCH_TUNABLEOP_ENABLED=1
 
 # "From ComfyUI README: You can enable experimental memory efficient attention with pytorch 2.5/ComfyUI/RDNA3"
-#ENV TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
-#python main.py --use-pytorch-cross-attention
+#export TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
+#python main.py --listen --use-pytorch-cross-attention
 
 # DEBUG
 #exec python3 -c 'import torch' 2> /dev/null && echo 'Success' || echo 'Failure'
